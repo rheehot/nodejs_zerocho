9b58309fb79b4e2980084d506e8d1b7ff0e92a7b
jokh0108 <jokh0108@gmail.com>
2019-10-30 12:45:55 +0900

    add express middleware & seperate router

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const route = require('./route.js');
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'html')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'main.html'));
-});
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'about.html'));
-});
+app.get('/', route);
+app.use((req, res, next) => { // 404 처리 부분
+    res.status(404).send('일치하는 주소가 없습니다!');
+})
+app.use((err, req, res, next) => { // 에러 처리 부분
+    console.error(err.stack); // 에러 메시지 표시
+    res.status(500).send('서버 에러!'); // 500 상태 표시 후 에러 메시지 전송
+})
 app.listen(8080, () => {
   console.log('Express App on port 8080!');
 });
