08d84c3e13e4cd4042bd56e3d9aa898505b7fb1b
jokh0108 <jokh0108@gmail.com>
2019-10-30 19:38:24 +0900

    correct an error(mongoose.connect)

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
-const mongoose = require('moongoose');
+const mongoose = require('mongoose');
 module.exports = () => {
     function connect() {
-        mongoose.connect('localhost:27017', function(err){
-            if(err){
-                console.error('monngodb connection error', err);                
+        mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
+            if (err) {
+                console.error('mongodb connection error', err);
             }
             console.log('mongodb connected');
         });
     }
     connect();
     mongoose.connection.on('disconnected', connect);
-    require('./user.js');
+    require('./user.js'); // user.js는 나중에 만듭니다.
 };
