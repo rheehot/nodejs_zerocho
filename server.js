decaa2c9bb7e6c55dda4cab1b047a5bd26a7515d
jokh0108 <jokh0108@gmail.com>
2019-10-30 14:21:09 +0900

    modify typo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const route = require('./route.js');
 
 app.set('view engine', 'pug'); // (1) 엔진을 pug로 설정하는 부분
-app.set('view', path.join(__dirname, 'html')); // (2) pug 파일들이 있는 폴더를 정하는 부분
+app.set('views', path.join(__dirname, 'html')); // (2) pug 파일들이 있는 폴더를 정하는 부분
 
 app.use(express.static(path.join(__dirname, 'html')));
 app.use('/', route);
